9743cc70755d2f3b96803268e63320d757b41842
Archie <archie.yang@dynamocover.com>
2022-11-30 14:04:05 +0000

    add message part

--- double_yang/blog/views.py
+++ double_yang/blog/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
+import environ
+
+env = environ.Env()
+# reading .env file
+environ.Env.read_env()
+print(env('MAILGUN_API_KEY'))
+
+
+def send_simple_message(name, email, message):
+    return requests.post(
+        env('MAILGUN_URL'),
+        auth=("api", env('MAILGUN_API_KEY')),
+        data={"from": "{} <mailgun@{}>".format(name, env('SEND')),
+              "to": ["ayaung@163.com",],
+              "subject": "Double Yang website message",
+              "text": "name: " + name + "  email: " + email + "  " + message})
+
 
 def home(request):
-	return render(request, 'me.html')
+    if request.method == "POST":
+        print(request.POST)
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(name, email, message)
+        send_simple_message(name, email, message)
+        print("Message sent")
+    return render(request, 'me.html')
